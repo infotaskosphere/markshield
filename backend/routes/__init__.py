fa1832e450982cffd847c70a0b81f1d50ea40e14
infotaskosphere <info.taskosphere@gmail.com>
2026-03-13 19:43:47 +0530

    Create __init__.py

---
+++ backend/routes/__init__.py
@@ -0,0 +1,6 @@
+from .cause_list  import bp_cause
+from .application import bp_app
+from .agent       import bp_agent
+from .search      import bp_search
+from .efiling     import bp_efiling
+from .portfolio   import bp_portfolio
