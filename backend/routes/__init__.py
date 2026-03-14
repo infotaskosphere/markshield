c0d692e0416189c1765848a672901c6667b50393
infotaskosphere <info.taskosphere@gmail.com>
2026-03-14 08:01:15 +0530

    Update __init__.py

--- backend/routes/__init__.py
+++ backend/routes/__init__.py
@@ -1,6 +1,15 @@
-from .cause_list  import bp_cause
+from .cause_list import bp_cause
 from .application import bp_app
-from .agent       import bp_agent
-from .search      import bp_search
-from .efiling     import bp_efiling
-from .portfolio   import bp_portfolio
+from .agent import bp_agent
+from .search import bp_search
+from .efiling import bp_efiling
+from .portfolio import bp_portfolio
+
+**all** = [
+"bp_cause",
+"bp_app",
+"bp_agent",
+"bp_search",
+"bp_efiling",
+"bp_portfolio"
+]
